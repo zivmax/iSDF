9c96590feea8690f22ac7c071ccac9c20220784f
zivmax <zivmax@foxmail.com>
2024-07-14 09:06:22 +0000

    fix: support python 3.10

--- setup.py
+++ setup.py
@@ -1,21 +1,34 @@
+#!/usr/bin/env python
+
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-#!/usr/bin/env python
-
 from setuptools import setup
-import shlex
 import subprocess
-
+import datetime
 
 def git_version():
-    cmd = 'git log --format="%h" -n 1'
-    return subprocess.check_output(shlex.split(cmd)).decode()
+    try:
+        # Try to get the version using git describe
+        cmd = ['git', 'describe', '--tags', '--always']
+        version = subprocess.check_output(cmd, universal_newlines=True).strip()
+        
+        # If the output starts with a 'v', remove it
+        if version.startswith('v'):
+            version = version[1:]
+        
+        # Replace '-' with '.' to make it a valid version string
+        version = version.replace('-', '.')
+        
+        return version
+    except subprocess.CalledProcessError:
+        # If git command fails, use a date-based version
+        now = datetime.datetime.now(datetime.timezone.utc)
+        return now.strftime("%Y.%m.%d.%H%M%S")
 
-
-version = git_version()
+version = "1.0+" + git_version()
 
 setup(
     name='incSDF',
